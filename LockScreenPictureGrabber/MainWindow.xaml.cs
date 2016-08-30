ecd787bde4adf9a3e04b1055a43528474915be86
Jason Champion <jchampion@zetacentauri.com>
2016-08-30 14:32:04 -0700

    Code checkin.

---
+++ LockScreenPictureGrabber/MainWindow.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace LockScreenPictureGrabber
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            // http://lifehacker.com/how-to-save-the-windows-10-lock-screen-images-you-like-1768783711
+            String localPath = Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData);
+            String path = localPath + "\\Packages\\Microsoft.Windows.ContentDeliveryManager_cw5n1h2txyewy\\LocalState\\Assets";
+            label1.Content = path;
+            //String[] files = { };
+            DirectoryInfo di = new DirectoryInfo(path);
+            FileSystemInfo[] files = di.GetFileSystemInfos();
+            var orderedFiles = files.OrderBy(f => f.CreationTime);
+            foreach (FileSystemInfo file in files)
+            {
+                Image image = new Image();
+                BitmapImage bmp = new BitmapImage();
+                bmp.BeginInit();
+                bmp.UriSource = new Uri(file.FullName);
+                bmp.EndInit();
+                image.Source = bmp;
+                image.Width = 240;
+                if (image.Height > 300)
+                {
+                    image.Height = 300;
+                }
+                listBox1.Items.Add(image);
+            }
+            textBox1.Text = "Image.jpg";
+        }
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            String filename = ((BitmapImage)((Image)listBox1.SelectedItem).Source).UriSource.AbsolutePath;
+            String destinationPath = "C:\\Users\\Xangis\\Pictures\\" + textBox1.Text;
+            File.Copy(filename, destinationPath);
+        }
+    }
+}
--- README.md
+++ README.md
@@ -3,7 +3,8 @@ Lock Screen Picture Grabber
 
 This is a utility to retrieve Windows 10 Spotlight images from the lock screen and save
 them in your Pictures folder. It opens the images in the hidden data folder and lets you
-pick which ones to save.
+pick which ones to save (you'll need to set the filename before clicking save for a
+selected image, and it's too dumb to ask you whether you want to overwrite an exsiting image).
 
 It has not been tested on computers other than my own. The code project is a Visual Studio 2010
 WPF project, but should work fine on newer versions.
@@ -11,4 +12,8 @@ WPF project, but should work fine on newer versions.
 A prebuilt executable is in the Release folder. If you just want to try the app, get and run that
 file.
 
-Let me know if you have any issues.
+This software is released under the MIT license.
+
+Let me know if you have any issues. Pull requests accepted.
+
+![Lock Screen Picture Grabber](https://github.com/Xangis/LockScreenPictureGrabber/images/LockScreenImageSaver.png)
